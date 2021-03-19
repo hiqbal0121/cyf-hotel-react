81b5b0733e2204746136386a1c1f25168c6d340b
Haroon <h.iqbal0121@gmail.com>
2021-03-19 14:38:01 +0000

    Lesson 1 - 1,2 & 3

--- src/App.js
+++ src/App.js
@@ -6,10 +6,87 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
+      <TouristInfoCards />
       <Bookings />
+      <Footer />
     </div>
   );
 };
+
+function Heading() {
+  return (
+    <div className="HeaderBoth">
+      <img
+        className="Logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="logo"
+        height="50"
+        width="50"
+      />
+      <header className="App-header">CYF Hotel</header>
+    </div>
+  );
+}
+
+function TouristInfoCards() {
+  return (
+    <div className="card" class="row">
+      <div className="card1" class="col-sm-4">
+        <img
+          src="https://i1.wp.com/www.hisour.com/wp-content/uploads/2018/05/Architecture-in-Glasgow.jpg?w=1024&ssl=1"
+          className="card-img-top"
+          height="300"
+        />
+        <div className="card-body" class="col text-center">
+          <p>Glasgow is lovely! Isn't it?</p>
+          <a href="https://peoplemakeglasgow.com/" className="btn btn-primary">
+            Visit Glasgow
+          </a>
+        </div>
+      </div>
+      <div className="card2" class="col-sm-4">
+        <img
+          src="https://www.aboutmanchester.co.uk/wp-content/uploads/2020/06/DDCCD1FC-B7A7-4939-9EE1-8BD9274D8CCB.jpeg"
+          className="card-img-top"
+          height="300"
+        />
+        <div className="card-body" class="col text-center">
+          <p>Manchester is lovely! I think?</p>
+          <a href="https://visitmanchester.com" className="btn btn-primary">
+            Visit Manchester
+          </a>
+        </div>
+      </div>
+      <div className="card3" class="col-sm-4">
+        <img
+          src="https://london.ac.uk/sites/default/files/styles/promo_large/public/2018-10/london-aerial-cityscape-river-thames_1.jpg?itok=BMaDUhjp"
+          className="card-img-top"
+          height="300"
+        />
+        <div className="card-body" class="col text-center">
+          <p>London is lovely! Said no one. Ever.</p>
+          <a href="https://visitlondon.com/" className="btn btn-primary">
+            Visit London
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+// const FooterArray = [
+//   "123 Fake Street, London, E1 4UD",
+//   "hello@fakehotel.com",
+//   "0123 456789",
+// ];
 
 export default App;
